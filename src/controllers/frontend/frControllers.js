4fc7bf490cd952716caa35b445d4cd7ad0d90370
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2023-05-16 16:20:24 +0300

    //

--- src/controllers/frontend/frControllers.js
+++ src/controllers/frontend/frControllers.js
@@ -8,7 +8,7 @@ const getHomePage =  (req, res, next) =>{
         const response =  axios.get("https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=29lQ4dwCehTz8q61Y7jNtx71Cgq6YSun");
         response.then((result) => {
         var value = result.data.results;
-        
+        console.log(result.data.results.lists[0].books[1]);
 
         res.render("index", {api:value, layout: false});
     }).catch((err) => {
