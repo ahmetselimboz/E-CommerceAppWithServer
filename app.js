a947b34de9679f8cebfa535d23cb6c4c2d72515b
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2023-05-31 00:16:12 +0300

    ..

--- app.js
+++ app.js
@@ -1,80 +1,80 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const path = require('path');
-const flash = require('connect-flash');
-const session = require('express-session')
-const passport = require('passport');
+require("dotenv").config();
+const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 
-
-const ejs = require('ejs');
-const expressLayouts = require('express-ejs-layouts');
+const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, "./src/views/frontend"));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./src/views/frontend"));
 
 //require('./src/config/bookApi');
 
-
-require('./src/config/database');
-const MongoDBStore = require('connect-mongodb-session')(session);
+require("./src/config/database");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const sessionStore = new MongoDBStore({
-    uri: process.env.MONGODB_CONNECTION_STRING,
-    collection: "sessions"
-})
+  uri: process.env.MONGODB_CONNECTION_STRING,
+  collection: "sessions",
+});
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-
-        maxAge: 24*60*60*1000
-
+      maxAge: 24 * 60 * 60 * 1000,
     },
-    store: sessionStore
-}))
+    store: sessionStore,
+  })
+);
 
 
 
 app.use(flash());
-app.use((req,res,next)=>{
-    res.locals.validation_error = req.flash("validation_error");
-    res.locals.success_message = req.flash("success_message");
-    res.locals.email = req.flash("email");
-    res.locals.name = req.flash("name");
-    res.locals.surname = req.flash("surname");
-    res.locals.login_error = req.flash('error');
-    next();
-})
+app.use((req, res, next) => {
+  res.locals.validation_error = req.flash("validation_error");
+  res.locals.success_message = req.flash("success_message");
+  res.locals.email = req.flash("email");
+  res.locals.name = req.flash("name");
+  res.locals.surname = req.flash("surname");
+  res.locals.login_error = req.flash("error");
+  next();
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-const frRouter = require('./src/routers/frontend/frRouters');
-const authRouter = require('./src/routers/frontend/authRouters');
+const frRouter = require("./src/routers/frontend/frRouters");
+const authRouter = require("./src/routers/frontend/authRouters");
 const mobile_frRouter = require("./src/routers/mobile/mobile_frRouters");
 const mobile_authRouter = require("./src/routers/mobile/mobile_authRouters");
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+    
 
-app.get("/", (req, res, next) =>{
-   res.redirect("/homepage")
-})
 
-app.use('/', frRouter);
-app.use('/auth', authRouter);
-app.use("/mobile" , mobile_frRouter);
-app.use("/mobile/auth" , mobile_authRouter);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is standing to ${process.env.PORT} port`);
-})
+
+app.get("/", (req, res, next) => {
+  res.redirect("/homepage");
+});
+
+app.use("/", frRouter);
+app.use("/auth", authRouter);
+app.use("/mobile", mobile_frRouter);
+app.use("/mobile/auth", mobile_authRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is standing to ${process.env.PORT} port`);
+});
