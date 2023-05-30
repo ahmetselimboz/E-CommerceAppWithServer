4b3ffaa7a0df130df063f38207137c223d4eb2ec
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2023-05-30 22:47:59 +0300

    //

--- src/controllers/mobile/mobile_authController.js
+++ src/controllers/mobile/mobile_authController.js
@@ -3,93 +3,79 @@ const User = require("../../models/userModel");
 const passport = require("passport");
 require("../../config/passport_local")(passport);
 const bcrypt = require("bcrypt");
-const nodemailer = require('nodemailer');
-const jsonwebtoken = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const jsonwebtoken = require("jsonwebtoken");
 const { log } = require("console");
 const { boolean } = require("webidl-conversions");
 
-
-
- const getFalseLogin = (req, res, next) => {
-  
-};
- const getTrueLogin = (req, res, next) => {
- 
-};
+const getFalseLogin = (req, res, next) => {};
+const getTrueLogin = (req, res, next) => {};
 
 const postLogin = (req, res, next) => {
- console.log("geldim");
-    try {
-      passport.authenticate("local", function (err, user, info) {
-        //console.log(req);
-        //console.log(user);
+  console.log("geldim");
+  try {
+
+
+    passport.authenticate("local", function (err, user, info) {
+      
+     
+      //console.log(req);
+      //console.log(user);
+      if (err) {
+        //console.log("cp1");
+        //console.log(err);
+      }
+      if (!user) {
+        console.log("geldimfalse");
+        var status = false;
+        var truemesaj = "";
+
+        var falseUser = {
+          _id: "",
+          name: "",
+          surname: "",
+          email: "",
+          emailIsActive: false,
+          password: "",
+          createdAt: "",
+          updatedAt: "",
+          __v: 0,
+        };
+
+        
+        return res.json({
+          durum: status,
+          user: falseUser,
+          mesaj: info.message,
+        });
+        
+      }
+
+      req.logIn(user, function (err) {
+        console.log("cp1");
         if (err) {
-          //console.log("cp1");
+          //console.log("cp3");
           //console.log(err);
-          
-        }
-        if (!user) {
-          console.log("geldimfalse");
-          var status = false;
-          var truemesaj = ""
-        
-          var falseUser = {
-              _id: "",
-              name: "",
-              surname: "",
-              email: "",
-              emailIsActive: false,
-              password: "",
-              createdAt: "",
-              updatedAt: "",
-              __v: 0
-          }
-        
+        } else {
+          console.log("geldimtrue");
+          var status = true;
+          var truemesaj = " ";
           //console.log(res.locals.login_error[0] );
           return res.json({
             durum: status,
-            user: falseUser,
-            mesaj: truemesaj
-          })
+            user: req.user,
+            mesaj: truemesaj,
+          });
         }
-    
-        req.logIn(user, function (err) {
-          console.log("cp1");
-          if (err) {
-            //console.log("cp3");
-            //console.log(err);
-           
-          }else{
-            console.log("geldimtrue");
-            var status = true;
-            var truemesaj = ""
-            //console.log(res.locals.login_error[0] );
-            return res.json({
-              durum: status,
-              user: req.user,
-              mesaj: truemesaj
-            })
-          }
-          
-         
-        });
-      })(req, res, next);
+      });
+    })(req, res, next);
 
-
-
-
-
-        // passport.authenticate("local", {
-        //     successRedirect: "auth/truelogin",
-        //     failureRedirect: "auth/falselogin",
-        //     failureFlash: true,
-        //   })(req, res, next);
-        
-    } catch (error) {
-        
-    }
-   
-  
+    // passport.authenticate("local", {
+    //     successRedirect: "auth/truelogin",
+    //     failureRedirect: "auth/falselogin",
+    //     failureFlash: true,
+    //   })(req, res, next);
+  } catch (error) {}
 };
 
 const getRegister = (req, res, next) => {
@@ -101,78 +87,80 @@ const postRegister = async (req, res, next) => {
   console.log("istek geldi");
   console.log(req.body);
   var deger = false;
-    try {
-      const _user = await User.findOne({ email: req.body.email });
+  try {
+    const _user = await User.findOne({ email: req.body.email });
 
-      if (_user && _user.emailIsActive == true) {
-        req.flash("validation_error", [{ msg: "Bu mail zaten kayıtlı" }]);
-        req.flash("email", req.body.email);
-        req.flash("name", req.body.name);
-        req.flash("surname", req.body.surname);
-        res.redirect("/auth/register");
-      } else if ((_user && _user.emailIsActive == false) || _user == null) {
-        if (_user) {
-          await User.findByIdAndRemove({ _id: _user.id });
-        }
+    if (_user && _user.emailIsActive == true) {
+      req.flash("validation_error", [{ msg: "Bu mail zaten kayıtlı" }]);
+      req.flash("email", req.body.email);
+      req.flash("name", req.body.name);
+      req.flash("surname", req.body.surname);
+      res.redirect("/auth/register");
+    } else if ((_user && _user.emailIsActive == false) || _user == null) {
+      if (_user) {
+        await User.findByIdAndRemove({ _id: _user.id });
+      }
 
-        const newUser = new User({
-          name: req.body.name,
-          surname: req.body.surname,
-          email: req.body.email,
-          password: await bcrypt.hash(req.body.password, 10),
-        });
+      const newUser = new User({
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        password: await bcrypt.hash(req.body.password, 10),
+      });
 
-        await newUser.save();
-        console.log("kullanici kaydedildi");
-        deger=true;
-        const jwtInfo ={
-          id: newUser.id,
-          email: newUser.email
-        }
+      await newUser.save();
+      console.log("kullanici kaydedildi");
+      deger = true;
+      const jwtInfo = {
+        id: newUser.id,
+        email: newUser.email,
+      };
 
-        const jwtToken = jsonwebtoken.sign(jwtInfo, process.env.CONFIRM_MAIL_JWT_SECRET , {expiresIn: "1d"});
+      const jwtToken = jsonwebtoken.sign(
+        jwtInfo,
+        process.env.CONFIRM_MAIL_JWT_SECRET,
+        { expiresIn: "1d" }
+      );
 
-        const url = process.env.WEB_SITE_URL + "auth/verify?id=" + jwtToken;
+      const url = process.env.WEB_SITE_URL + "auth/verify?id=" + jwtToken;
 
-        let transporter = nodemailer.createTransport({
-          service:"gmail",
-          auth:{
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASSWORD
-          }
-        })
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_PASSWORD,
+        },
+      });
 
-        await transporter.sendMail({
+      await transporter.sendMail(
+        {
           from: "Kitap Dağı <info@kitapdagi.com>",
           to: newUser.email,
           subject: "Emailinizi lütfen onaylayın",
-          text: "Emailiniz onaylamak için lütfen şu linke tıklayın: "+ url
-        },(error, info)=>{
+          text: "Emailiniz onaylamak için lütfen şu linke tıklayın: " + url,
+        },
+        (error, info) => {
           if (error) {
             console.log("Sending mail error: " + error);
           }
           console.log("Mail sended");
           console.log(info);
           transporter.close();
-        })
-res.json(deger);
-
-        //req.flash("success_message", [{ msg: "Lütfen mail kutunuzu kontrol ediniz" }]);
-
-        //res.redirect("/auth/login");
-      }
-    } catch (error) {
-      deger=false;
+        }
+      );
       res.json(deger);
+
+      //req.flash("success_message", [{ msg: "Lütfen mail kutunuzu kontrol ediniz" }]);
+
+      //res.redirect("/auth/login");
     }
-  
+  } catch (error) {
+    deger = false;
+    res.json(deger);
+  }
 };
 
-const emailVerify = (req,res,next)=>{
-
-}
-
-
+const emailVerify = (req, res, next) => {};
 
 const getLogOut = (req, res, next) => {
   req.logout(function (err) {
